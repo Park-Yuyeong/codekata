857de7a35c4386fb54298e31c532c58b3c36fd68
Park-Yuyeong <dbduda1@kookmin.ac.kr>
2024-05-31 09:31:28 +0900

    feat: add gugudan.js

---
+++ gugudan.js
@@ -0,0 +1,8 @@
+function gugudan() {
+  for (let i = 2; i <= 9; i++) {
+    for (let j = 2; j <= 9; j++) {
+      console.log(`${i} * ${j} = ${i * j}`);
+    }
+    console.log("===========");
+  }
+}
